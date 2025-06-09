aa79b0b1578bea53e60c2c1f74692c60be6091eb
Manish <133939770+jaxckk@users.noreply.github.com>
2025-06-09 17:20:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ContactForm from './Components/ContactForm/ContactForm'
+import Navigation from './Components/Navigation/Navigation'
+import ContactHeader from './Components/ContactHeader/ContactHeader'
+
+
+function App() {
+  return ( <div>
+    <Navigation/>
+    <ContactHeader/>
+    <ContactForm/>
+  </div>
+
+  )
+}
+
+export default App
